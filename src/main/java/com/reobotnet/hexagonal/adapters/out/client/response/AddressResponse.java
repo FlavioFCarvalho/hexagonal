1323f3379a75045db61957668cfc0a5332c6337e
FlavioFCarvalho <flaviocfr@gmail.com>
2025-03-25 17:28:47 -0300

    Criando o Client para a busca de endereço.

---
+++ src/main/java/com/reobotnet/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,12 @@
+package com.reobotnet.hexagonal.adapters.out.client.response;
+
+
+public class AddressResponse {
+
+    private String street;
+	
+	private String city;
+	
+	private String state;
+	
+}
