393db8316b991c649bfecc05d7cbc7b11d6a3b11
FlavioFCarvalho <flaviocfr@gmail.com>
2025-03-27 09:34:15 -0300

    Criando o adapter do delete

---
+++ src/main/java/com/reobotnet/hexagonal/adapters/out/DeleteCustomerByIdAdaptar.java
@@ -0,0 +1,18 @@
+package com.reobotnet.hexagonal.adapters.out;
+
+import com.reobotnet.hexagonal.adapters.out.repository.CustomerRepository;
+import com.reobotnet.hexagonal.aplication.ports.out.DeleteCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCustomerByIdAdaptar implements DeleteCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
